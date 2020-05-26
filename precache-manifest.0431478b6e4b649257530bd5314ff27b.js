535598b592dbf43cdbc10fed0bf22e3c19902e18
marian_moise <marian_moise@ymail.com>
2020-05-26 18:08:13 +0300

    Updates

---
+++ precache-manifest.0431478b6e4b649257530bd5314ff27b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea909d81651e850930ab15e1762262bd",
+    "url": "/ReactForceConverter/index.html"
+  },
+  {
+    "revision": "ebe99c94b12765757cf1",
+    "url": "/ReactForceConverter/static/css/main.166c36e3.chunk.css"
+  },
+  {
+    "revision": "85658517955bdd54c9d5",
+    "url": "/ReactForceConverter/static/js/2.a687ed9d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactForceConverter/static/js/2.a687ed9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebe99c94b12765757cf1",
+    "url": "/ReactForceConverter/static/js/main.8b23b748.chunk.js"
+  },
+  {
+    "revision": "d5cafc4fd22ca3199a50",
+    "url": "/ReactForceConverter/static/js/runtime-main.2db88ed9.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ReactForceConverter/precache-manifest.0431478b6e4b649257530bd5314ff27b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactForceConverter/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
